d0e5857441a4e84137d2ebba1502d9a757484aa7
Daria Shenkevych <daria.shenkevych@gmail.com>
2022-12-14 01:02:34 +0200

    toastify bug fix
    styles added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { GlobalStyle } from 'components/GlobalStyle';
 import { Box } from '../Box';
